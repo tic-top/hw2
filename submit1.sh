681ad1b0276472608799b12db37e317fc8a2287c
kirp@umich.edu <tic-top>
2024-10-01 16:46:07 +0000

    hi

--- submit1.sh
+++ submit1.sh
@@ -5,11 +5,11 @@
 #SBATCH --job-name=mpi_hw2
 #SBATCH --mail-type=BEGIN,END
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=4
+#SBATCH --ntasks-per-node=1
 #SBATCH --mem-per-cpu=1g
 #SBATCH --time=00:05:00
 #SBATCH --account=cse587f24_class
 #SBATCH --partition=standard
 
-mpirun -np 4 --bind-to core:overload-allowed ./main 2000 500 0 > out.txt
-mpirun -np 4 --bind-to core:overload-allowed ./main 1000 4000 0 > out.txt
+mpirun -np 1 --bind-to core:overload-allowed ./main 2000 500 0 > out11.txt
+mpirun -np 1 --bind-to core:overload-allowed ./main 1000 4000 0 > out12.txt
