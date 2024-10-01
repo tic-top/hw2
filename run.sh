5224d73e34ce995df295bf86c206d7b9aea02a8a
kirp@umich.edu <tic-top>
2024-10-01 19:51:01 +0000

    hi

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 #bin/bash
+module load gcc
+module load openmpi
+mpic++ main.cpp -o main
 sbatch serial.sh
 sbatch submit1.sh
-sbatch submit2.sh
 sbatch submit4.sh
 sbatch submit16.sh
 sbatch submit36.sh
