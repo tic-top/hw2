ce71ea0acd84414a175a9e882506bf9268463b2d
kirp@umich.edu <tic-top>
2024-10-01 17:02:39 +0000

    .

--- 4t.sh
+++ 4t.sh
@@ -5,7 +5,7 @@
 #SBATCH --job-name=mpi_hw2
 #SBATCH --mail-type=BEGIN,END
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=4
+#SBATCH --ntasks-per-node=2
 #SBATCH --mem-per-cpu=1g
 #SBATCH --time=00:05:00
 #SBATCH --account=linmacse2
