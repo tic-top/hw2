219db5ca1c77add0048d49ef685e637111f029ef
kirp@umich.edu <tic-top>
2024-10-01 16:02:05 +0000

    hi

--- submit_1.sh
+++ submit_1.sh
@@ -11,5 +11,5 @@
 #SBATCH --account=cse587f24_class
 #SBATCH --partition=standard
 
-mpirun -np 1 --bind-to core:overload-allowed ./main 2000 500 > out.txt
-mpirun -np 1 --bind-to core:overload-allowed ./main 1000 4000 > out.txt
+mpirun -np 1 --bind-to core:overload-allowed ./main 2000 500 0 > out.txt
+mpirun -np 1 --bind-to core:overload-allowed ./main 1000 4000 0 > out.txt
