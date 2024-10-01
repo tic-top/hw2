538c11e468d8829ddd69642db79c5965f39b0fb8
kirp@umich.edu <tic-top>
2024-10-01 16:47:35 +0000

    test

---
+++ 4t.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# (See https://arc-ts.umich.edu/greatlakes/user-guide/ for command details)
+# Set up batch job settings
+
+#SBATCH --job-name=mpi_hw2
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=4
+#SBATCH --mem-per-cpu=1g
+#SBATCH --time=00:05:00
+#SBATCH --account=linmacse2
+#SBATCH --partition=standard
+
+mpirun -np 4 --bind-to core:overload-allowed ./main 400 400 0 > test.txt &
